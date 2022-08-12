887731ff4ee3a76aed1d96173348d9fca52cf3eb
Edgard <starkuryliat@gmail.com>
2022-08-12 12:47:29 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Header } from './component/Layout/Header';
+import { Footer } from './component/Layout/Footer';
+import { Main } from './component/Layout/Main';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
